a0769e3f82648adc71bfca3c08cbe3b783f687cf
Dilli_Rao <101472151+legendarycopyninja@users.noreply.github.com>
2023-01-31 15:22:05 +0530

    trial commit

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -3,7 +3,7 @@
 export default function Header() {
     return (
         <div className="header">
-            <img src="/src/assets/profile.jpg" alt="dilli profile picture" className="profilepic" />
+            <img src="profile.jpg" alt="dilli profile picture" className="profilepic" />
             <div className="devinfo">
                 <h2>Dilli Rao D</h2>
                 <h3>Front-End Developer</h3>
@@ -17,7 +17,7 @@ export default function Header() {
                 <button  className="email"><img src="/src/assets/mail.png" alt="email" />Email</button>
             </a>
             <a href="https://www.linkedin.com/in/dilli-rao-dadi-578493180/" target="_blank" rel="noopener noreferrer" >
-                 <button className="linkedin"> <img src="/src/assets/linkedin.png" alt="linkedin" />LinkedIn</button>
+                 <button className="linkedin"> <img src="linkedin.png" alt="linkedin" />LinkedIn</button>
             </a>
 
             </div>
